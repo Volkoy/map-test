9ad3e15ff549a7dc66e50ab8db405e313244cc37
Vadym Volkovinskyy <cyred3@gmail.com>
2025-08-12 02:05:02 +0100

    formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "<TARGET_URL>",
-    },
-  },
-})
+});
